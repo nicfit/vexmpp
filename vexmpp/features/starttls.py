92dfa2546fdb72ce71b2f5755e1c23d774ca68d2
Travis Shirk <travis@pobox.com>
2015-05-08 09:51:03 -0600

    Sync latest

--- vexmpp/features/starttls.py
+++ vexmpp/features/starttls.py
@@ -11,17 +11,21 @@ NS_URI = "urn:ietf:params:xml:ns:xmpp-tls"
 def handle(stream, feature_elem, timeout=None):
     nsmap = {"tls": NS_URI}
 
-    required = ("{%s}required" % NS_URI) in [c.tag for c in feature_elem]
+    required = isRequired(feature_elem)
 
     stream.send(Stanza("starttls", nsmap={None: NS_URI}))
     resp = yield from stream.wait([("/tls:proceed", nsmap),
                                    ("/tls:failure", nsmap)], timeout=timeout)
     if resp.name == "{%s}proceed" % NS_URI:
         yield from stream._transport.starttls()
-        return True
     else:
+        # A real stanza/stream error type is not wrapped by the <failure/>,
+        # unlike other newer protocols, so gin up a dummy.
         raise XmppError("starttls failure: %s" % resp.toXml().decode())
 
-    return False
+    return True
 
+
+def isRequired(feature_elem):
+    return ("{%s}required" % NS_URI) in [c.tag for c in feature_elem]
 
