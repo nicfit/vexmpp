f95cf0618816f1c1566b711a8fc591c20b9d3d44
nicfit <travis@pobox.com>
2015-05-23 22:25:05 -0600

    kick

--- vexmpp/protocols/muc.py
+++ vexmpp/protocols/muc.py
@@ -6,7 +6,7 @@ from lxml import etree
 from .. import stream
 from ..utils import xpathFilter
 from ..jid import Jid as BaseJid
-from ..stanzas import Presence, Iq
+from ..stanzas import Presence, Iq, ElementWrapper
 
 '''
 Multi-user chat (aka XEP 45)
@@ -50,60 +50,6 @@ def errorPresenceXpath(nick_jid):
     '''Error from the room or service.'''
     xpath = "/presence[@from='{}'][@type='error']".format(nick_jid.bare)
     return (xpath, None)
-
-
-@asyncio.coroutine
-def enterRoom(stream, room, service, nick, password=None,
-              config_new_room_callback=None, timeout=None):
-    nick_jid = MucJid((room, service, nick))
-
-    room_jid = nick_jid.room_jid
-
-    pres = Presence(to=nick_jid)
-
-    x = etree.SubElement(pres.xml, "{%s}x" % NS_URI, nsmap={None: NS_URI})
-    if password:
-        pw = etree.SubElement(x, "password")
-        pw.text = password
-
-    stream.send(pres)
-
-    pres = yield from stream.wait([selfPresenceXpath(nick_jid),
-                                   errorPresenceXpath(nick_jid)],
-                                  timeout=timeout)
-    if pres.error:
-        raise pres.error
-
-    ROOM_CREATED_PRESENCE_XPATH = ("/presence/mu:x/mu:status[@code='201']",
-                                   {"mu": NS_URI_USER})
-    ROOM_OWNER_PRESENCE_XPATH = \
-            ("/presence/mu:x/mu:item[@affiliation='owner']",
-             {"mu": NS_URI_USER})
-
-    # Owner create operations
-    if (pres.xml.xpath(ROOM_OWNER_PRESENCE_XPATH[0],
-                       namespaces=ROOM_OWNER_PRESENCE_XPATH[1]) and
-        pres.xml.xpath(ROOM_CREATED_PRESENCE_XPATH[0],
-                       namespaces=ROOM_CREATED_PRESENCE_XPATH[1])):
-
-        if config_new_room_callback is None:
-            # New instant room, accept the default config
-            iq = Iq(to=room_jid, type="set", request=("query", NS_URI_OWNER))
-            x = etree.SubElement(iq.query, "{jabber:x:data}x",
-                                 nsmap={None: "jabber:x:data"})
-            x.attrib["type"] = "submit"
-            yield from stream.sendAndWait(iq, raise_on_error=True,
-                                          timeout=timeout)
-        else:
-            # Configure room
-            iq = Iq(to=room_jid, type="get", request=("query", NS_URI_OWNER))
-            room_config = yield from stream.sendAndWait(iq, raise_on_error=True,
-                                                        timeout=timeout)
-            room_config = config_new_room_callback(room_config)
-            yield from stream.sendAndWait(room_config, raise_on_error=True,
-                                          timeout=timeout)
-
-    return pres
 
 
 class RosterItem:
@@ -205,3 +151,68 @@ class MucMixin(stream.Mixin):
             if self_presence:
                 del self._muc_rooms[room_jid]
 
+
+@asyncio.coroutine
+def enterRoom(stream, room, service, nick, password=None,
+              config_new_room_callback=None, timeout=None):
+    nick_jid = MucJid((room, service, nick))
+
+    room_jid = nick_jid.room_jid
+
+    pres = Presence(to=nick_jid)
+
+    x = etree.SubElement(pres.xml, "{%s}x" % NS_URI, nsmap={None: NS_URI})
+    if password:
+        pw = etree.SubElement(x, "password")
+        pw.text = password
+
+    stream.send(pres)
+
+    pres = yield from stream.wait([selfPresenceXpath(nick_jid),
+                                   errorPresenceXpath(nick_jid)],
+                                  timeout=timeout)
+    if pres.error:
+        raise pres.error
+
+    ROOM_CREATED_PRESENCE_XPATH = ("/presence/mu:x/mu:status[@code='201']",
+                                   {"mu": NS_URI_USER})
+    ROOM_OWNER_PRESENCE_XPATH = \
+            ("/presence/mu:x/mu:item[@affiliation='owner']",
+             {"mu": NS_URI_USER})
+
+    # Owner create operations
+    if (pres.xml.xpath(ROOM_OWNER_PRESENCE_XPATH[0],
+                       namespaces=ROOM_OWNER_PRESENCE_XPATH[1]) and
+        pres.xml.xpath(ROOM_CREATED_PRESENCE_XPATH[0],
+                       namespaces=ROOM_CREATED_PRESENCE_XPATH[1])):
+
+        if config_new_room_callback is None:
+            # New instant room, accept the default config
+            iq = Iq(to=room_jid, type="set", request=("query", NS_URI_OWNER))
+            x = etree.SubElement(iq.query, "{jabber:x:data}x",
+                                 nsmap={None: "jabber:x:data"})
+            x.attrib["type"] = "submit"
+            yield from stream.sendAndWait(iq, raise_on_error=True,
+                                          timeout=timeout)
+        else:
+            # Configure room
+            iq = Iq(to=room_jid, type="get", request=("query", NS_URI_OWNER))
+            room_config = yield from stream.sendAndWait(iq, raise_on_error=True,
+                                                        timeout=timeout)
+            room_config = config_new_room_callback(room_config)
+            yield from stream.sendAndWait(room_config, raise_on_error=True,
+                                          timeout=timeout)
+
+    return pres
+
+
+@asyncio.coroutine
+def kick(stream, nick_jid, reason, timeout=None):
+    iq = Iq(to=nick_jid.room_jid, type="set", request=("query", NS_URI_ADMIN))
+    query = ElementWrapper(iq.query)
+    item = ElementWrapper(query.appendChild("item"))
+    item.set("nick", nick_jid.nick)
+    item.set("role", "none")
+    query.appendChild("reason").text = reason
+
+    _ = yield from stream.sendAndWait(iq, raise_on_error=True, timeout=timeout)
