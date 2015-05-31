9da84bdb53cb89a0f9b3a0681302039d22b18b85
Travis Shirk <travis@pobox.com>
2015-05-31 10:27:00 -0600

    Removed _getAttr

--- vexmpp/features/stream_mgmt.py
+++ vexmpp/features/stream_mgmt.py
@@ -12,7 +12,7 @@ def handle(stream, feature_elem, sm_opts, timeout=None):
 
     enable_elem = Stanza("enable", nsmap={None: NS_URI})
     if sm_opts and sm_opts.resume:
-        enable_elem._setAttr("resume", "true")
+        enable_elem.set("resume", "true")
     stream.send(enable_elem)
 
     resp = yield from stream.wait([("/sm:enabled", nsmap),
@@ -20,10 +20,10 @@ def handle(stream, feature_elem, sm_opts, timeout=None):
     if resp.name == "{%s}failed" % NS_URI:
         raise makeStanzaError(resp.xml)
 
-    sm_opts.sm_id = resp._getAttr("id")
-    sm_opts.resume = bool(resp._getAttr("resume") and
-                          resp._getAttr("resume") in ("1", "true"))
-    sm_opts.resume_location = resp._getAttr("location")
-    sm_opts.max_resume_time = resp._getAttr("max")
+    sm_opts.sm_id = resp.get("id")
+    sm_opts.resume = bool(resp.get("resume") and
+                          resp.get("resume") in ("1", "true"))
+    sm_opts.resume_location = resp.get("location")
+    sm_opts.max_resume_time = resp.get("max")
 
     return True
